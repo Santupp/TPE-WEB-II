d5bca461ff776f5d328c5e3c4f2644edc02a6a19
Santinou <santino.monniello@gmail.com>
2024-10-15 12:13:19 -0300

    changed database

--- tpe.sql
+++ tpe.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 16, 2024 at 12:33 AM
+-- Generation Time: Oct 13, 2024 at 11:40 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,30 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tpe`
+-- Database: `db_tareas`
 --
+--
+CREATE DATABASE IF NOT EXISTS `tpe` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `tpe`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `foto_url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`, `foto_url`) VALUES
+(3, 'Sci-fi', 'https://www.imdb.com/title/tt4154756/');
 
 -- --------------------------------------------------------
 
@@ -37,7 +59,8 @@ CREATE TABLE `director` (
 --
 
 INSERT INTO `director` (`id`, `nombre`) VALUES
-(3, 'aaa');
+(3, 'aaa'),
+(4, 'Christopher Nolan');
 
 -- --------------------------------------------------------
 
@@ -50,12 +73,49 @@ CREATE TABLE `peliculas` (
   `nombre` varchar(255) NOT NULL,
   `fecha_estreno` date DEFAULT NULL,
   `genero` varchar(255) DEFAULT NULL,
-  `id_director` int(11) DEFAULT NULL
+  `id_director` int(11) DEFAULT NULL,
+  `id_categoria` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `peliculas`
+--
+
+INSERT INTO `peliculas` (`id`, `nombre`, `fecha_estreno`, `genero`, `id_director`, `id_categoria`) VALUES
+(5, 'Interstellar', '2014-11-06', 'SCI-FI', 4, 3),
+(6, 'el señor de los anillos', '2024-10-02', 'sci-fi', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `email` varchar(250) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(60) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `email`, `password`, `username`) VALUES
+(1, 'nico@web2.com', '$2y$10$xQop0wF1YJ/dKhZcWDqHceUM96S04u73zGeJtU80a1GmM.H5H0EHC', ''),
+(2, '', '$2y$10$XJjarJMOLhdRCYgycMxaG.uzrX6mqYXv/MgVa6POvHFpjuw4Don2G', 'webadmin');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
 
 --
 -- Indexes for table `director`
@@ -68,22 +128,42 @@ ALTER TABLE `director`
 --
 ALTER TABLE `peliculas`
   ADD UNIQUE KEY `id` (`id`),
-  ADD KEY `id_director` (`id_director`);
+  ADD KEY `id_director` (`id_director`),
+  ADD KEY `id_categoria` (`id_categoria`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `director`
 --
 ALTER TABLE `director`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `peliculas`
 --
 ALTER TABLE `peliculas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
@@ -94,7 +174,8 @@ ALTER TABLE `peliculas`
 -- Constraints for table `peliculas`
 --
 ALTER TABLE `peliculas`
-  ADD CONSTRAINT `peliculas_ibfk_1` FOREIGN KEY (`id_director`) REFERENCES `director` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `peliculas_ibfk_1` FOREIGN KEY (`id_director`) REFERENCES `director` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `peliculas_ibfk_2` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
